9ebe7ccd3ace8cd8c0b2a98cff8681f7def7ccc7
Sergiu Chiuchiu <sergiu.chiuchiu@gmail.com>
2020-05-10 01:50:28 +0300

    gcp

--- src/main/java/ro/gcloud/mycinema/MycinemaApplication.java
+++ src/main/java/ro/gcloud/mycinema/MycinemaApplication.java
@@ -1,13 +1,19 @@
 package ro.gcloud.mycinema;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
 public class MycinemaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MycinemaApplication.class, args);
     }
